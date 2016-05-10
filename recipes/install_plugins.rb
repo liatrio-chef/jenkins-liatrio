20de7ad3db7d2f6ef49f2a95281e556c0ef1e0ba
Drew Holt <drew@liatrio.com>
2016-05-09 17:24:05 -0700

    seperating install_plugins.rb

--- recipes/install_plugins.rb
+++ recipes/install_plugins.rb
@@ -10,63 +10,6 @@ node[:jenkins_liatrio][:install_plugins][:plugins_list].each do |i|
   jenkins_plugin i
 end
 
-cookbook_file '/var/lib/jenkins/sonar-runner-dist-2.4.zip' do
-  source 'sonar-runner-dist-2.4.zip'
-  owner node[:jenkins][:master][:user]
-  group node[:jenkins][:master][:group]
-  mode '0644'
-  action :create
-end
-
-execute 'unzip_sonar-runner' do
-  command 'cd /var/lib/jenkins; unzip sonar-runner-dist-2.4.zip'
-  not_if do ::File.exists?('/var/lib/jenkins/sonar-runner-2.4') end
-end
-
-template '/var/lib/jenkins/hudson.plugins.sonar.SonarPublisher.xml' do
-  source   'var/lib/jenkins/hudson.plugins.sonar.SonarPublisher.xml.erb'
-  mode     '0644'
-  owner node[:jenkins][:master][:user]
-  group node[:jenkins][:master][:group]
-  variables({})
-end
-
-template '/var/lib/jenkins/hudson.plugins.sonar.SonarRunnerInstallation.xml' do
-  source   'var/lib/jenkins/hudson.plugins.sonar.SonarRunnerInstallation.xml.erb'
-  mode     '0644'
-  owner node[:jenkins][:master][:user]
-  group node[:jenkins][:master][:group]
-  variables({})
-end
-
-template '/var/lib/jenkins/sonar-runner-2.4/conf/sonar-runner.properties' do
-  source   'var/lib/jenkins/sonar-runner-2.4/conf/sonar-runner.properties.erb'
-  mode     '0644'
-  owner node[:jenkins][:master][:user]
-  group node[:jenkins][:master][:group]
-  variables({})
-end
-
-cookbook_file '/var/lib/jenkins/hygieia-publisher.hpi' do
-  source 'hygieia-publisher.hpi'
-  mode     '0644'
-  owner node[:jenkins][:master][:user]
-  group node[:jenkins][:master][:group]
-  action :create
-end
-
-execute 'install-plugin-hygieia-publisher' do
-  command "java -jar /opt/jenkins-cli.jar -s http://#{node[:jenkins][:master][:host]}:#{node[:jenkins][:master][:port]}/ install-plugin /var/lib/jenkins/hygieia-publisher.hpi"
-  not_if ("java -jar /opt/jenkins-cli.jar -s http://#{node[:jenkins][:master][:host]}:#{node[:jenkins][:master][:port]}/ list-plugins| grep hygieia-publisher")
-end
-
-template '/var/lib/jenkins/jenkins.plugins.hygieia.HygieiaPublisher.xml' do
-  source   'var/lib/jenkins/jenkins.plugins.hygieia.HygieiaPublisher.xml.erb'
-  mode     '0644'
-  owner node[:jenkins][:master][:user]
-  group node[:jenkins][:master][:group]
-  variables({})
-end
 
 # link maven
 template '/var/lib/jenkins/hudson.tasks.Maven.xml' do
@@ -76,15 +19,4 @@ template '/var/lib/jenkins/hudson.tasks.Maven.xml' do
   owner node[:jenkins][:master][:user]
   group node[:jenkins][:master][:group]
   notifies :restart, 'service[jenkins]', :delayed
-end
-
-if node[:jenkins_liatrio][:install_plugins][:enablearchiva] == true 
-  template '/var/lib/jenkins/.m2/settings.xml' do
-    source   'var/lib/jenkins/.m2/settings.xml.erb'
-    mode '0644'
-    owner node[:jenkins][:user]
-    group node[:jenkins][:group]
-    variables({})
-    notifies :restart, 'service[jenkins]', :delayed
-  end
 end
---
+++ recipes/install_plugins_hygieia.rb
@@ -0,0 +1,28 @@
+#
+# Cookbook Name:: jenkins-liatrio
+# Recipe:: install_plugins_hygieia
+#
+# Author: Drew Holt <drew@liatrio.com>
+#
+
+cookbook_file '/var/lib/jenkins/hygieia-publisher.hpi' do
+  source 'hygieia-publisher.hpi'
+  mode     '0644'
+  owner node[:jenkins][:master][:user]
+  group node[:jenkins][:master][:group]
+  action :create
+end
+
+execute 'install-plugin-hygieia-publisher' do
+  command "java -jar /opt/jenkins-cli.jar -s http://#{node[:jenkins][:master][:host]}:#{node[:jenkins][:master][:port]}/ install-plugin /var/lib/jenkins/hygieia-publisher.hpi"
+  not_if ("java -jar /opt/jenkins-cli.jar -s http://#{node[:jenkins][:master][:host]}:#{node[:jenkins][:master][:port]}/ list-plugins| grep hygieia-publisher")
+end
+
+template '/var/lib/jenkins/jenkins.plugins.hygieia.HygieiaPublisher.xml' do
+  source   'var/lib/jenkins/jenkins.plugins.hygieia.HygieiaPublisher.xml.erb'
+  mode     '0644'
+  owner node[:jenkins][:master][:user]
+  group node[:jenkins][:master][:group]
+  variables({})
+end
+
