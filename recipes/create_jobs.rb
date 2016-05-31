dd8e826178ba9161731d5e02a7904a6f84d73991
Drew Holt <drew@liatrio.com>
2016-05-31 09:42:49 -0700

    THEIA-10 renaming file had issues, creating new create_jobs.rb with create_job.rb content

---
+++ recipes/create_jobs.rb
@@ -0,0 +1,63 @@
+#
+# Cookbook Name:: jenkins-liatrio
+# Attributes:: create_jobs
+#
+# Author: Drew Holt <drew@liatrio.com>
+#
+
+# Create petclinic-simple job
+template '/var/lib/jenkins/jobs/petclinic-simple-config.xml' do
+  source 'var/lib/jenkins/jobs/petclinic-simple-config.xml.erb'
+  mode     '0644'
+  owner node[:jenkins][:master][:user]
+  group node[:jenkins][:master][:group]
+  variables({
+    :nexus_repo => node[:jenkins_liatrio][:nexus_repo]
+  })
+end
+
+jenkins_job 'petclinic-simple-auto-1' do
+  config '/var/lib/jenkins/jobs/petclinic-simple-config.xml'
+end
+
+# Create deploy-tomcat job
+template '/var/lib/jenkins/jobs/deploy-tomcat-config.xml' do
+  source 'var/lib/jenkins/jobs/deploy-tomcat-config.xml.erb'
+  mode     '0644'
+  owner node[:jenkins][:master][:user]
+  group node[:jenkins][:master][:group]
+  variables({
+  })
+end
+
+jenkins_job 'deploy-tomcat' do
+  config '/var/lib/jenkins/jobs/deploy-tomcat-config.xml'
+end
+
+# selenium-chrome-petclinic-test
+template '/var/lib/jenkins/jobs/selenium-chrome-petclinic-test-config.xml' do
+  source 'var/lib/jenkins/jobs/selenium-chrome-petclinic-test-config.xml.erb'
+  mode     '0644'
+  owner node[:jenkins][:master][:user]
+  group node[:jenkins][:master][:group]
+  variables({
+  })
+end
+
+jenkins_job 'selenium-chrome-petclinic-test' do
+  config '/var/lib/jenkins/jobs/selenium-chrome-petclinic-test-config.xml'
+end
+
+# selenium-firefox-petclinic-test
+template '/var/lib/jenkins/jobs/selenium-firefox-petclinic-test-config.xml' do
+  source 'var/lib/jenkins/jobs/selenium-firefox-petclinic-test-config.xml.erb'
+  mode     '0644'
+  owner node[:jenkins][:master][:user]
+  group node[:jenkins][:master][:group]
+  variables({
+  })
+end
+
+jenkins_job 'selenium-firefox-petclinic-test' do
+  config '/var/lib/jenkins/jobs/selenium-firefox-petclinic-test-config.xml'
+end
