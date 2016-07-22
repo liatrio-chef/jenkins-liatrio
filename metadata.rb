4f2a17cc68b0f46dffdd6944c5459c8c92272466
Drew Holt <drew@liatrio.com>
2016-07-21 17:03:25 -0700

    THEIA-59 bumping to latest version of jenkins for this wrapper

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures jenkins-liatrio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 
-depends 'jenkins', '~> 2.4.1'
+depends 'jenkins', '~> 2.6.0'
