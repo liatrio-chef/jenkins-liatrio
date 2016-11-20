2146bcc6f53b449f68297e625d19abfa5bb16c72
Drew Holt <drew@liatrio.com>
2016-11-19 22:02:22 -0700

    LTA-42 use jenkins groovy to configure global env allowing us to not manage config.xml any more

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'drew@liatrio.com'
 license          'All rights reserved'
 description      'Installs/Configures jenkins-liatrio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.3.2'
+version          '1.3.3'
 
 depends 'java'
 depends 'jenkins', '~> 2.6.0'
