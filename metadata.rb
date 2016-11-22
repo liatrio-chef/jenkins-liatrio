adbd08d4398fb77ec855abd7cfea5e2fced41256
Drew Holt <drew@liatrio.com>
2016-11-21 18:22:56 -0700

    LTA-42 add spork-config.yml, manually bumped cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'drew@liatrio.com'
 license          'All rights reserved'
 description      'Installs/Configures jenkins-liatrio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.3.3'
+version          '1.3.4'
 
 depends 'java'
 depends 'jenkins', '~> 2.6.0'
