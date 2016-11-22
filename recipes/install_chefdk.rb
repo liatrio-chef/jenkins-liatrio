4f304425ab737e3f6a1ae63547ae69228f25c42b
Drew Holt <drew@liatrio.com>
2016-11-21 18:11:09 -0700

    LTA-42 install knife-spork and slack-notifier gems

--- recipes/install_chefdk.rb
+++ recipes/install_chefdk.rb
@@ -10,3 +10,13 @@ execute 'install chefdk' do
   command 'rpm -Uvh https://packages.chef.io/stable/el/7/chefdk-0.16.28-1.el7.x86_64.rpm'
   not_if 'rpm -qa chefdk | grep chefdk'
 end
+
+execute 'install knife-spork gem for chef omnibus' do
+  command 'chef gem install --no-user-install knife-spork'
+  not_if 'chef gem list | grep knife-spork'
+end
+
+execute 'install slack-notifier gem for chef omnibus' do
+  command 'chef gem install --no-user-install slack-notifier'
+  not_if 'chef gem list | grep slack-notifer'
+end
