782efbd2ff723fd1e250c3073b3a37f97039205b
Drew Holt <drew@liatrio.com>
2016-11-23 21:35:29 -0700

    LTA-42 fix yum-epel dependency in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures jenkins-liatrio'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.4.2'
 
-depends 'java'
+depends 'java', '~> 1.42.0'
 depends 'jenkins', '~> 2.6.0'
 depends 'apache2', '~> 3.2.2'
-depends 'yum-epel', '~> 0.7.0'
+depends 'yum-epel', '~> 1.0.2'
