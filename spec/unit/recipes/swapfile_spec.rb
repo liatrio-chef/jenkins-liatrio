701aefad645d430c5e722808490aacf6f9b9215e
Tyler Gering <tyler@liatrio.com>
2017-02-18 13:40:33 -0800

    wrote swapfile spec

---
+++ spec/unit/recipes/swapfile_spec.rb
@@ -0,0 +1,23 @@
+#
+# ChefSpec for jenkins-liatrio
+# Recipe: swapfile
+# Author: Tyler Gering tyler@liatrio.com
+#
+
+require 'spec_helper'
+
+describe 'jenkins-liatrio::swapfile' do
+  let(:chef_run) { ChefSpec::SoloRunner.new.converge(described_recipe) }
+
+  it 'Creates Swapfile' do
+    expect(chef_run).to run_bash('create_swapfile')
+  end
+
+  it 'Enables Mount' do
+    expect(chef_run).to enable_mount('/dev/null')
+  end
+
+  it 'Activates Swap' do
+    expect(chef_run).to run_bash('activate_swap')
+  end
+end
