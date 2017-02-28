4246346df5a41f9d565b725151e98e1bdf34b132
Tyler Gering <tyler@liatrio.com>
2017-02-28 14:53:28 -0800

    successfully stubbed service

--- spec/unit/recipes/plugin_slack_spec.rb
+++ spec/unit/recipes/plugin_slack_spec.rb
@@ -7,10 +7,8 @@
 # Currently this recipe doesn't have anything running in it.
 require 'spec_helper'
 
-
-
 describe 'jenkins-liatrio::plugin_slack' do
-  let(:chef_run) { ChefSpec::SoloRunner.new.converge(jenkins-liatrio::default,described_recipe) }
+  let(:chef_run) { ChefSpec::SoloRunner.new.converge('jenkins-liatrio::default', described_recipe) }
 
   before do
     Chef::EncryptedDataBagItem.stub(:load).with('jenkins', 'cred').and_return('slack_token' => 'slack_token',
@@ -30,8 +28,6 @@ describe 'jenkins-liatrio::plugin_slack' do
 
   it 'Restarts Jenkins' do
     resource = chef_run.template('/var/lib/jenkins/jenkins.plugins.slack.SlackNotifier.xml')
-    expect(resource).to notify('jenkins').to('restart').delayed
-
+    expect(resource).to notify('service[jenkins]').to('restart').delayed
   end
-
 end
